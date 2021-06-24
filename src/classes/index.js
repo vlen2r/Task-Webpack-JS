a28f467bf651712a710c182947717ff34b7ea27d
Vlen2R <71619929+vlen2r@users.noreply.github.com>
2021-06-24 11:51:49 -0300

    Ruta de Clases
    Creamos la ruta de clases, lo cual nos permite importar 1 sola linea en el ./index.js

---
+++ src/classes/index.js
@@ -0,0 +1,7 @@
+import { TaskList } from "./task-list.class";
+import { Task } from "./task.class";
+
+export {
+    TaskList,
+    Task
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import "./styles.css"; //Este include nos permite exportarlo al dist como un componente aparte y no en el main.js
+import { Task, TaskList } from "./classes";
